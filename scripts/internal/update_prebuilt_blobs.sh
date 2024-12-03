8b8235b43f789afde68ed4321176b39f7a287a70
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-12-03 22:46:03 +0100

    scripts: internal: update blobs: switch to BTE for S908B
    EUX still not receiving it

--- scripts/internal/update_prebuilt_blobs.sh
+++ scripts/internal/update_prebuilt_blobs.sh
@@ -101,7 +101,7 @@ case "$1" in
         ;;
     "unica/patches/uwb/essi")
         MODULE="$1"
-        FW="SM-S908B/EUX/350048581234569"
+        FW="SM-S908B/BTE/350048581234569"
         BLOBS="$(find "$SRC_DIR/unica/patches/uwb/essi/system" -type f | sed "s.$SRC_DIR/unica/patches/uwb/essi.system.")"
         BLOBS+="$(find "$SRC_DIR/unica/patches/uwb/essi/system_ext" -type f -printf "\n%p" | sed "s.$SRC_DIR/unica/patches/uwb/essi/.." | sed 's/system_ext/system\/system\/system_ext/' )"
         ;;
