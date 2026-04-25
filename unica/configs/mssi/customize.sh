b9d4869b262acbd06367ee9e4a02b84563a72b6c
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-04-25 13:29:58 +0000

    mssi: use tflite videoclassifier

--- unica/configs/mssi/customize.sh
+++ unica/configs/mssi/customize.sh
@@ -213,6 +213,7 @@ DELETE_FROM_WORK_DIR "system" "system/lib64/libtensorflowlite_inference_api.myfi
 DELETE_FROM_WORK_DIR "system" "system/lib64/libdualcam_portraitlighting_gallery_360_lite.so"
 
 LIBS="
+system/lib64/libVideoClassifier.camera.samsung.so
 system/lib64/libsaiv_HprFace_cmh_support_jni.camera.samsung.so
 system/lib64/libImageTagger.camera.samsung.so
 system/lib64/libStride.camera.samsung.so
@@ -265,7 +266,6 @@ for lib in $LIBS; do
 done
 
 LIBS="
-system/lib64/libVideoClassifier.camera.samsung.so
 system/lib64/libhybridHDR_wrapper.camera.samsung.so
 system/lib64/libhybrid_high_dynamic_range.arcsoft.so
 system/lib64/libSwIsp_core.camera.samsung.so
