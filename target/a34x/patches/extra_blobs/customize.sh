f56be71a2fc930b606e04fb06763fb155b8e7fa2
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-04-25 10:43:30 +0000

    a34x: enable V901 imagetagger

--- target/a34x/patches/extra_blobs/customize.sh
+++ target/a34x/patches/extra_blobs/customize.sh
@@ -2,3 +2,9 @@ LOG_STEP_IN "- Adding Pet Clustering blobs"
 ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/pet_detector"
 ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/pet_mypetsearch"
 LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding V901 ImageTagger blobs"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/aig_document_detector"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/aig_document_classifier"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/aig_classifier"
+LOG_STEP_OUT
--- target/a34x/patches/stock_blobs/customize.sh
+++ target/a34x/patches/stock_blobs/customize.sh
@@ -13,4 +13,3 @@ HEX_PATCH "$WORK_DIR/system/system/lib64/libmidas_core.camera.samsung.so" \
     "726f2e70726f647563742e646576696365" "726f2e6d696461732e6465766963650000"
 LOG_STEP_OUT
 
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
