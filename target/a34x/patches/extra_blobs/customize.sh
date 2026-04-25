d4e3b9712bb9d763a802947a90571d559f69d3ee
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-04-25 08:32:48 +0000

    a34x: enable pet clustering

---
+++ target/a34x/patches/extra_blobs/customize.sh
@@ -0,0 +1,4 @@
+LOG_STEP_IN "- Adding Pet Clustering blobs"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/pet_detector"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/pet_mypetsearch"
+LOG_STEP_OUT
