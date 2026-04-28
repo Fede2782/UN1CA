4a97f39c7ce23605eba3e122d454ad5ba52c38e8
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-04-28 17:14:35 +0000

    unica: mods: paradigm: move Galaxy AI Pet Detector to paradigm
    * It's a external feature to S22 so it makes more sense

--- unica/patches/stock_blobs/customize.sh
+++ unica/patches/stock_blobs/customize.sh
@@ -139,11 +139,6 @@ if [ ! -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/hs_segmenter" ]; the
     ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/saiv/image_understanding/db/hs_segmenter/hs_segmenter.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
     ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/saiv/image_understanding/db/hs_segmenter/hs_segmenter.info" 0 0 644 "u:object_r:vendor_configs_file:s0"
 fi
-if [ ! -d "$WORK_DIR/vendor/etc/petdetector/studio_pd" ]; then
-    ADD_TO_WORK_DIR "gts11xx" "vendor" "etc/petdetector/studio_pd/config_thresholds.json" 0 0 644 "u:object_r:vendor_configs_file:s0"
-    ADD_TO_WORK_DIR "gts11xx" "vendor" "etc/petdetector/studio_pd/studio_pd_cnn.info" 0 0 644 "u:object_r:vendor_configs_file:s0"
-    ADD_TO_WORK_DIR "gts11xx" "vendor" "etc/petdetector/studio_pd/studio_pd_cnn.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
-fi
 if [ -f "$WORK_DIR/system/system/lib64/libSmartScan.camera.samsung.so" ]; then
     ADD_TO_WORK_DIR "a34xxx" "vendor" "etc/saiv/image_understanding/db/SS_segmenter/SS_segmenter_cnn.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
     ADD_TO_WORK_DIR "a34xxx" "vendor" "etc/saiv/image_understanding/db/SS_segmenter/SS_segmenter_cnn.info" 0 0 644 "u:object_r:vendor_configs_file:s0"
