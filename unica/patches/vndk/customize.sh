0f564bb2315e30020109abe34ddd16df3f6816c8
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-05-29 21:51:10 +0200

    unica: patches: vndk: remove unsupported VNDK versions
    * Only VNDK 33 and VNDK 31 are supported in One UI 8.5
    
    * VNDK 30, VNDK 32 and VNDK 34 (64-32) aren't supported anymore
      in One UI 8.5

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -6,14 +6,8 @@ fi
 # [
 ADD_TARGET_VNDK_APEX() {
     case "$TARGET_BOARD_API_LEVEL" in
-        "30")
-            ADD_TO_WORK_DIR "a73xqxx" "system_ext" "apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
-            ;;
         "31")
             ADD_TO_WORK_DIR "r11sxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
-            ;;
-        "32")
-            ADD_TO_WORK_DIR "b4qxxx" "system_ext" "apex/com.android.vndk.v32.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
         "33")
             ADD_TO_WORK_DIR "dm1qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
